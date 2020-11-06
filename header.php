9f1e3f5b50c8b0c84b2cf50fa676a3b8497f2f57
Richie <r.withers@sculpt.digital>
2020-11-06 12:04:39 +0000

    test style

--- header.php
+++ header.php
@@ -2,8 +2,9 @@
 <html>
 <head>
     <title>Richie Synoptic Project</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
 </head>
 <body>
+    test
 </body>
 </html>
